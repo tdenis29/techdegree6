45307ba53ac9c79f6bffed0b9188dbdbf58cb02f
tdenis29 <tdenis29@outlook.com>
2021-03-01 13:17:32 -0800

    plans layout

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Global Variables 
 const keyBoard = document.getElementById('qwerty');       //Keys user selects
 const phrase = document.getElementById('phrase');          //random phrase to guess
-const resetButton = document.getElementById('btn-reset');   //reset the game 
+const startButton = document.getElementById('btn-reset');   //reset/start the game 
 let score = 0;                                                //score
 
 const phraseArr = [ 
@@ -12,3 +12,37 @@ const phraseArr = [
     "buy the ticket take the ride"
 ]
 
+//return random phrase from array 
+const getRandomPhraseArray = arr => {
+
+
+}
+
+
+
+//add the letters of a string to the display
+const addPhraseToDisplay = arr => {
+
+
+}
+
+//check if selected letter is in phrase
+const checkLetter = button => {
+
+}
+
+//check if game has been won or lost 
+const checkWin = () => {
+
+}
+
+//listen for start button to fire 
+startButton.addEventListener("click", (e) => {
+
+
+});
+
+// listen for keyboard clicks
+qwerty.addEventListener('click', (e) => {
+
+});
