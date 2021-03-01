c59caedfc94e53db67c64e337441800fcd15bf8f
tdenis29 <tdenis29@outlook.com>
2021-03-01 13:08:14 -0800

    consts and phrase array

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Global Variables 
+const keyBoard = document.getElementById('qwerty');       //Keys user selects
+const phrase = document.getElementById('phrase');          //random phrase to guess
+const resetButton = document.getElementById('btn-reset');   //reset the game 
+let score = 0;                                                //score
+
+const phraseArr = [ 
+    "Ba Ram You",
+    "These are not the droids youre looking for",
+    "Lannisters always pay their debts",
+    "One Ring to rule them all",
+    "Would it save you a lot of time if I just gave up and went mad now?"
+]
+
